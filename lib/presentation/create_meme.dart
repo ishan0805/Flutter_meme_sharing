ff7124184732e105a9e589b0a6940eee4376c201
Ishan Kumar Srivastava <57336738+ishan0805@users.noreply.github.com>
2023-02-27 11:11:37 +0530

    changes done

---
+++ lib/presentation/create_meme.dart
@@ -0,0 +1,201 @@
+import 'package:crio_meme_sharing_app/bloc/meme_bloc.dart';
+import 'package:crio_meme_sharing_app/core/failures.dart';
+import 'package:crio_meme_sharing_app/core/providers.dart';
+import 'package:crio_meme_sharing_app/repository/models/meme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive/hive.dart';
+
+class CreateMeme extends StatefulWidget {
+  CreateMeme({Key? key}) : super(key: key);
+
+  @override
+  _CreateMemeState createState() => _CreateMemeState();
+}
+
+class _CreateMemeState extends State<CreateMeme> {
+  final _formkey = GlobalKey<FormState>();
+
+  MemeBloc? _memeBloc;
+  String? name, url, description;
+  @override
+  void initState() {
+    super.initState();
+    _memeBloc = context.read(memeBlocProvider);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var deviceSize = MediaQuery.of(context).size;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Create Meme'),
+        elevation: 0,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Container(
+              width: deviceSize.width < 500
+                  ? deviceSize.width / 1.2
+                  : deviceSize.width / 2.5,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            ' Create Meme',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20),
+                          ),
+                          Spacer(),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Form(
+                              key: _formkey,
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: <Widget>[
+                                  /*TextFormField(
+                                    key: ValueKey('name'),
+                                    validator: (value) {
+                                      if (value.isEmpty) {
+                                        return 'everyone has a name ';
+                                      }
+                                      return null;
+                                    },
+                                    onSaved: (value) {
+                                      name = value;
+                                    },
+                                    decoration: InputDecoration(
+                                      icon:
+                                          Icon(Icons.perm_contact_cal_outlined),
+                                      labelText: 'What we call you ?',
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(15),
+                                      ),
+                                    ),
+                                  ),*/
+                                  SizedBox(height: 10),
+                                  TextFormField(
+                                    key: ValueKey('url'),
+                                    validator: (value) {
+                                      if (value == null) {
+                                        return 'url is required';
+                                      }
+                                      return null;
+                                    },
+                                    onSaved: (value) {
+                                      url = value ?? "";
+                                    },
+                                    decoration: InputDecoration(
+                                      icon: Icon(Icons.link),
+                                      labelText: 'url to awesomeness',
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(15),
+                                      ),
+                                    ),
+                                    keyboardType: TextInputType.url,
+                                  ),
+                                  SizedBox(height: 10),
+                                  TextFormField(
+                                    key: ValueKey('caption'),
+                                    validator: (value) {
+                                      if (value == null) {
+                                        return "Don\'t be shy speak up";
+                                      }
+                                      return null;
+                                    },
+                                    onSaved: (value) {
+                                      description = value ?? "";
+                                    },
+                                    decoration: InputDecoration(
+                                      icon: Icon(Icons.message),
+                                      labelText: 'what is all this about ?',
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(15),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              width: deviceSize.width / 4,
+              height: deviceSize.height / 15,
+              child: ElevatedButton(
+                // RoundedRectangleBorder(
+                //    borderRadius: BorderRadius.circular(15),
+                //  ),
+                style: TextButton.styleFrom(primary: Colors.teal),
+                onPressed: () async {
+                  if (_formkey.currentState!.validate()) {
+                    _formkey.currentState!.save();
+                    //print("here");
+                    //print(description);
+                    //name = await Hive.box('user').get('email');
+                    Memes meme = Memes(url: url, caption: description);
+
+                    final response = (await _memeBloc!.postMeme(meme))
+                        .fold((l) => l, (r) => r);
+                    showError(response, context);
+                    Navigator.pop(context);
+                  }
+                },
+                child: Text(
+                  "Submit",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+void showError(Object response, BuildContext context) {
+  if (response is Failures) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(response.message),
+      ),
+    );
+  } else {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: const Text("Success"),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/home.dart
@@ -0,0 +1,496 @@
+import 'package:crio_meme_sharing_app/core/providers.dart';
+import 'package:crio_meme_sharing_app/utilies/size_config.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/src/provider.dart';
+
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final _formkey = GlobalKey<FormState>();
+  var size = SizeConfig();
+  bool isOnSignIn = false;
+  TextEditingController passwordSignUp = TextEditingController();
+  TextEditingController passwordConfirmSignUp = TextEditingController();
+  TextEditingController emailSignUp = TextEditingController();
+  TextEditingController emailSignIn = TextEditingController();
+  TextEditingController passwordSignIn = TextEditingController();
+  TextEditingController nameSignUp = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    size.init(context); //Initializing size of page
+
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(SizeConfig.inputSpacing / 1.9),
+        child: SizedBox(
+          height: SizeConfig.screenHeight,
+          child: Row(
+            children: [
+              SizeConfig.screenWidth > 800 ? sideView() : SizedBox(),
+              isOnSignIn ? SignIn() : SignUp()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // ignore: non_constant_identifier_names
+  Expanded SignUp() {
+    return Expanded(
+      flex: 2,
+      child: Container(
+        color: Color(0xFF303031),
+        child: Padding(
+          padding: EdgeInsets.all(SizeConfig.blockSizeHorizontal),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Sign Up",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: SizeConfig.blockSizeVertical * 3,
+              ),
+              Text(
+                "Enter your details",
+              ),
+              SizedBox(
+                height: SizeConfig.blockSizeVertical * 7,
+              ),
+              Form(
+                key: _formkey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    TextFormField(
+                      controller: nameSignUp,
+                      validator: (value) {
+                        if (value == null || value == '') {
+                          return 'required';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.email),
+                        labelText: 'Name',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: SizeConfig.safeBlockVertical,
+                    ),
+                    TextFormField(
+                      controller: emailSignUp,
+                      validator: (value) {
+                        if (value == null || value == '') {
+                          return 'required';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.email),
+                        labelText: 'Email',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: SizeConfig.safeBlockVertical,
+                    ),
+                    // enter password
+                    TextFormField(
+                      controller: passwordSignUp,
+                      obscureText: true,
+                      keyboardType: TextInputType.visiblePassword,
+                      validator: (value) {
+                        if (value == null || value == '') {
+                          return 'Enter a Password';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.lock),
+                        labelText: 'Password',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: SizeConfig.safeBlockVertical,
+                    ),
+                    // confirm password
+                    TextFormField(
+                      controller: passwordConfirmSignUp,
+                      obscureText: true,
+                      keyboardType: TextInputType.visiblePassword,
+                      validator: (value) {
+                        if (value == null || value == '') {
+                          return 'Enter the above password';
+                        }
+                        if (passwordSignUp.text != passwordConfirmSignUp.text) {
+                          return 'Passwords do not matched';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.lock),
+                        labelText: 'Confirm Password',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: SizeConfig.blockSizeVertical * 6,
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(
+                    SizeConfig.safeBlockHorizontal, 0, 10, 0),
+                child: Container(
+                  height: 40,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      if (_formkey.currentState!.validate()) {
+                        final result = await context
+                            .read(authBlocProvider)
+                            .signUp(nameSignUp.text, emailSignUp.text,
+                                passwordConfirmSignUp.text);
+                        result.fold((l) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text(l.message)),
+                          );
+                        }, (r) {
+                          Navigator.pushNamed(context, '/feeds');
+                        });
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xFF34b996),
+                    ),
+                    child: Text("Sign Up"),
+                  ),
+                ),
+              ),
+              // for mobile view logic SignUp
+              SizeConfig.screenWidth > 800
+                  ? Container()
+                  : Center(
+                      child: TextButton(
+                        onPressed: () {
+                          setState(() {
+                            if (isOnSignIn == true) {
+                              isOnSignIn = false;
+                            } else {
+                              isOnSignIn = true;
+                            }
+                          });
+                        },
+                        child: Padding(
+                            padding: EdgeInsets.only(
+                                left: SizeConfig.safeBlockHorizontal,
+                                top: SizeConfig.safeBlockVertical),
+                            child: switchBetweenSignInSignUp()),
+                      ),
+                    ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // SignIn Widget
+  Expanded SignIn() {
+    return Expanded(
+      flex: 2,
+      child: Container(
+        child: Card(
+          color: Color(0xFF303031),
+          child: Padding(
+            padding: EdgeInsets.all(SizeConfig.blockSizeHorizontal * 2),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Sign In ",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: SizeConfig.blockSizeVertical * 3,
+                ),
+                Text(
+                  "Enter your details",
+                  //style: TextStyle(fontSize: 20),
+                ),
+                SizedBox(
+                  height: SizeConfig.blockSizeVertical * 7,
+                ),
+                Form(
+                  key: _formkey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      TextFormField(
+                        controller: emailSignIn,
+                        validator: (value) {
+                          if (value == null || value == '') {
+                            return 'Required';
+                          }
+                          if (!value.contains('@'))
+                            return "Enter a Valid email";
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.email),
+                          labelText: 'Email',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      TextFormField(
+                        controller: passwordSignIn,
+                        validator: (value) {
+                          if (value == null || value == '') {
+                            return 'Password';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.lock),
+                          labelText: 'Password',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                        keyboardType: TextInputType.url,
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: SizeConfig.blockSizeVertical * 2,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Checkbox(value: false, onChanged: (v) {}),
+                        Text('Remember Me'),
+                      ],
+                    ),
+                    // logiv for mobile view
+                    TextButton(
+                        onPressed: () {
+                          if (isOnSignIn == true) {
+                            isOnSignIn = false;
+                          } else {
+                            isOnSignIn = true;
+                          }
+                        },
+                        child: switchBetweenSignInSignUp()),
+                  ],
+                ),
+                SizedBox(
+                  height: SizeConfig.blockSizeVertical * 6,
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(
+                      SizeConfig.safeBlockHorizontal, 0, 10, 0),
+                  child: Container(
+                    height: 40,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        if (_formkey.currentState!.validate()) {
+                          final result = await context
+                              .read(authBlocProvider)
+                              .login(emailSignIn.text, passwordSignIn.text);
+                          result.fold((l) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text(l.message)),
+                            );
+                          }, (r) {
+                            Navigator.pushNamed(context, '/feeds');
+                          });
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFF34b996),
+                      ),
+                      child: Text("Login"),
+                    ),
+                  ),
+                ),
+                SizeConfig.screenWidth > 800
+                    ? Container()
+                    : Center(
+                        child: TextButton(
+                          onPressed: () {
+                            setState(() {
+                              if (isOnSignIn == true) {
+                                isOnSignIn = false;
+                              } else {
+                                isOnSignIn = true;
+                              }
+                            });
+                          },
+                          child: Padding(
+                              padding: EdgeInsets.only(
+                                  left: SizeConfig.safeBlockHorizontal,
+                                  top: SizeConfig.safeBlockVertical),
+                              child: switchBetweenSignInSignUp()),
+                        ),
+                      ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Text switchBetweenSignInSignUp() {
+    if (isOnSignIn == true) {
+      return Text(
+        "dont't have a account ?",
+        style: TextStyle(
+            color: Colors.white, decoration: TextDecoration.underline),
+      );
+    } else {
+      return Text(
+        "Already have a account ?SignIn",
+        style: TextStyle(
+            color: Colors.white, decoration: TextDecoration.underline),
+      );
+    }
+  }
+
+  Expanded sideView() {
+    return Expanded(
+      flex: 3,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            begin: Alignment.bottomRight,
+            end: Alignment.topLeft,
+            colors: [
+              Color(0xFF22909f),
+              Color(0xFF24949d),
+              Color(0xFF279a9d),
+              Color(0xFF289e9b),
+              Color(0xFF2ba399),
+              Color(0xFF2da999),
+              Color(0xFF30af99),
+              Color(0xFF34b996),
+            ],
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: SizeConfig.blockSizeVertical * 5,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: SizeConfig.blockSizeHorizontal),
+              child: Text(
+                "</> MemeShare",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: SizeConfig.blockSizeVertical * 9,
+            ),
+            Padding(
+              padding:
+                  EdgeInsets.only(left: SizeConfig.blockSizeHorizontal * 5),
+              child: Text(
+                "Laught .",
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: SizeConfig.blockSizeVertical * 4,
+            ),
+            Padding(
+              padding:
+                  EdgeInsets.only(left: SizeConfig.blockSizeHorizontal * 5),
+              child: Text(
+                "Share .",
+                style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: SizeConfig.blockSizeVertical * 4,
+            ),
+            Padding(
+              padding:
+                  EdgeInsets.only(left: SizeConfig.blockSizeHorizontal * 5),
+              child: Text(
+                "Support .",
+                style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: SizeConfig.blockSizeVertical * 4,
+            ),
+            /* Padding(
+              padding:
+                  EdgeInsets.only(left: SizeConfig.blockSizeHorizontal * 7),
+              child: Text(
+                "Spreading happiness one meme at a time !!",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),*/
+            SizedBox(
+              height: SizeConfig.blockSizeVertical * 8,
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  if (isOnSignIn == true) {
+                    isOnSignIn = false;
+                  } else {
+                    isOnSignIn = true;
+                  }
+                });
+              },
+              child: Padding(
+                  padding: const EdgeInsets.only(left: 80),
+                  child: switchBetweenSignInSignUp()),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
